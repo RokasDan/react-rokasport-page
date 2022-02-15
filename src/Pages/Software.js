19d1fe53716b0f720af1b800ed6f8f8ecf65022b
RokasDan <rokas108@gmail.com>
2022-02-15 21:10:05 +0000

    Added more content to the software page

--- src/Pages/Software.js
+++ src/Pages/Software.js
@@ -15,19 +15,29 @@ const Software = () => {
       </p>
 
       <SoftwareLink projectName='Trolley VR' link='/audio' date='Currently Working On' description='
-      A multiplayer virtual reality game which recreates multiple variants of the famous trolley dilemma.'/>
+      A multiplayer virtual reality game which recreates multiple variants of the famous trolley dilemma.
+      Project is made with Unity3D and C#.'/>
 
       <SoftwareLink projectName='React Audio Player' link='/audio' date='2022-02-08' description='
       A customisable react.js audio player which can have multiple instances on one page.'/>
 
       <SoftwareLink projectName='Stealth Boy' link='/audio' date='2021-09-26' description='
-      A third person stealth platformer game made with Unity3D.'/>
+      A third person stealth platformer game made with Unity3D and C#.'/>
 
       <SoftwareLink projectName='PyFighter' link='/audio' date='2021-07-12' description='
-      A 2D game side-scroll platformer game where you fight the Computer Science school teachers
-      from University of Nottingham.'/>
+      A 2D side-scroll platforming fighting game made with Python coding environment.'/>
 
+      <SoftwareLink projectName='8-Bit Tron' link='/audio' date='2021-06-03' description='
+      A 16 note sequenced synthesizer with a miniature digital audio workstation made with Pure Data
+      visual coding environment.'/>
 
+      <SoftwareLink projectName='Traffic Police' link='/audio' date='2021-04-07' description='
+      A website with MySQL database which imitates a "Traffic Police" report system. This project
+      was made using HTML, CSS, PHP and MySQL.'/>
+
+      <SoftwareLink projectName='Color To Sound' link='/audio' date='2020-03-21' description='
+      A musical instrument which plays specific chords and drones according to a color being scanned.
+      This project was made using Arduino systems and Java coding environment.'/>
     </div>
   );
 }
