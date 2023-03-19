f4888124d991ed7982f1bae9e86b06ac90c35eb7
RokasDan <rokas108@gmail.com>
2023-03-19 22:31:51 +0000

    Fixed code block

--- src/Components/CodeBlock.js
+++ src/Components/CodeBlock.js
@@ -5,6 +5,7 @@ import SyntaxHighlighter from "react-syntax-highlighter";
 import {materialDark} from "react-syntax-highlighter/dist/cjs/styles/prism";
 import {atomDark} from "react-syntax-highlighter/dist/cjs/styles/prism";
 import 'prismjs/components/prism-python';
+import 'prismjs/components/prism-csharp';
 
 export const CodeBlock = ({codeString, language}) => (
   <SyntaxHighlighter language={language} style={atomDark} customStyle={{
